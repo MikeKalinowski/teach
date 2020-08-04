889a66d3b398808d3fef3cb739322f72c4cbf470
MikeKalinowski <michal.kalinowski90@gmail.com>
2020-08-04 20:25:02 +0200

    refactor: rm unnecessary type

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,11 +1,4 @@
-export interface ApiConfig {
-  spaceId: string;
-  accessToken: string;
-  environmentId: string;
-  baseURI: string;
-}
-
-export const apiConfig: ApiConfig = {
+export const apiConfig = {
   spaceId: "f02ft778pf3z",
   accessToken: "GyXkg9yqxrtMZcw0labND5XdArUaok0AFFtR4aCnZDk",
   environmentId: "master",
