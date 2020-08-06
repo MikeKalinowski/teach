e38e31c3754ab17b6b2fd4064862690640bb8c4f
MikeKalinowski <michal.kalinowski90@gmail.com>
2020-08-06 21:51:18 +0200

    Updates

---
+++ precache-manifest.b693fd93f9cc686233eee79e7ce2e9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473764caaec4a2cdfc302a07cac0c7af",
+    "url": "/teach/index.html"
+  },
+  {
+    "revision": "6b2192d87dfb92ce6483",
+    "url": "/teach/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "7138395295df986eba82",
+    "url": "/teach/static/js/2.cf86183b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teach/static/js/2.cf86183b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2192d87dfb92ce6483",
+    "url": "/teach/static/js/main.9fe94367.chunk.js"
+  },
+  {
+    "revision": "80ddc177494b3446dc12",
+    "url": "/teach/static/js/runtime-main.a9ccda28.js"
+  }
+]);
